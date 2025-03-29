6b6924be77eb9075bc19fcd91d887fdf6fd06ea8
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-03-28 21:57:14 -0300

    criaçao do main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+// codigo para tornar o calendario interativo
+// utilizando funçao declarativa
+function colorirDia(){
+    //bloco de variaveis    (escopo de funçao)
+    let days = document.getElementById('day').value;
+}
