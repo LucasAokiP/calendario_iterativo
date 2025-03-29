0c7923e1e627023885694652f4e8ebcd843cc051
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-03-28 22:09:53 -0300

    criaçao das funçoes dos botoes no main.JS

--- main.js
+++ main.js
@@ -3,4 +3,19 @@
 function colorirDia(){
     //bloco de variaveis    (escopo de funçao)
     let days = document.getElementById('day').value;
+    let color = document.getElementById('color').value;
+    let calendar = document.getElementById('calendario');
+
+    //bloco de balidaçao de informaçao
+    if(!days){
+        alert("Favor, informar um dia existente no calendario");
+    }
+    else{
+        if((days > 0) && (days <31)){
+            let td = calendar.getElementsByTagName('td');
+            td.style.backgroundColor = color;
+        }else{
+            alert("Favor, informar uma data constante no calnedario");
+        }
+    }
 }
